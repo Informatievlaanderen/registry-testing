8ebdf4091c311b8bb8f75e6f8aa835531030a692
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2019-10-15 18:10:44 +0200

    fix: add apikey

--- user-files/simulations/vnext/MunicipalitySimulation.scala
+++ user-files/simulations/vnext/MunicipalitySimulation.scala
@@ -10,10 +10,12 @@ import flood._
 import basisregisters._
 
 class MunicipalitySimulation extends Simulation {
+  val testApiKey = "cafebabe-1337-1337-1337-cdcdcdcdcdcd"
 
   // val httpProtocol = http
   val httpProtocol = httpConfigFlood
-    .baseUrl("https://api.staging-basisregisters.vlaanderen/v1")
+    .baseUrl("https://api.basisregisters.vlaanderen/v1")
+    .header("x-api-key", testApiKey)
     .warmUp("https://www.vlaanderen.be/nl")
     .acceptHeader("application/json, text/javascript, */*; q=0.5")
     .acceptLanguageHeader("en-US,en;q=0.9,nl;q=0.8")
@@ -37,5 +39,4 @@ class MunicipalitySimulation extends Simulation {
         .separatedByRampsLasting(10 seconds)
         .startingFrom(5)
     ).protocols(httpProtocol))
-
 }
