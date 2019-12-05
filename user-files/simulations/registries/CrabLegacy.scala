424d445812b3933ef16057929519c62eaa99c10f
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2019-12-05 16:27:11 +0100

    fix: add filtered list calls for crab legacy

--- user-files/simulations/registries/CrabLegacy.scala
+++ user-files/simulations/registries/CrabLegacy.scala
@@ -6,12 +6,16 @@ import infrastructure._
 import infrastructure.RegistryRequestChecks._
 
 object CrabLegacy {
-  private val crabHouseNumber = new CrabName("housenumber")
-  private val crabSubaddress = new CrabName("subaddress")
+  private val crabHouseNumber = new CrabName("housenumbers")
+  private val crabSubaddress = new CrabName("subaddresses")
+  private val housenumberFeeder = csv("all-crab-housenumber-ids.csv.zip").unzip.batch.random
+  private val subaddressFeeder = csv("all-crab-subaddress-ids.csv.zip").unzip.batch.random
   
   val possibleCalls = List(
-      Possibility(listHouseNumbers, 50),
-      Possibility(listSubaddresses, 50)
+      Possibility(listHouseNumbers, 10),
+      Possibility(filteredListHouseNumbers, 40),
+      Possibility(listSubaddresses, 10),
+      Possibility(filteredListSubaddresses, 40)
     )
     
   private def listHouseNumbers(responseTimes: MaximumResponseTimes) = {
@@ -21,6 +25,19 @@ object CrabLegacy {
         .check(
           status.isValidForList(crabHouseNumber),
           responseTimeInMillis.isValidForList(responseTimes, crabHouseNumber)
+        )
+    )
+  }
+
+  private def filteredListHouseNumbers(responseTimes: MaximumResponseTimes) = {
+    feed(housenumberFeeder)
+    .exec(
+      http(session => "Vraag alle subadres addressen op")
+        .get("/crabhuisnummers")
+        .queryParam("CrabHouseNumberId", "${HouseNumberId}")
+        .check(
+          status.isValidForFilteredList(crabHouseNumber),
+          responseTimeInMillis.isValidForFilteredList(responseTimes, crabHouseNumber)
         )
     )
   }
@@ -36,7 +53,21 @@ object CrabLegacy {
     )
   }
 
+  private def filteredListSubaddresses(responseTimes: MaximumResponseTimes) = {
+    feed(subaddressFeeder)
+    .exec(
+      http(session => "Vraag alle subadres addressen op")
+        .get("/crabsubadressen")
+        .queryParam("CrabSubaddressId", "${SubaddressId}")
+        .check(
+          status.isValidForFilteredList(crabSubaddress),
+          responseTimeInMillis.isValidForFilteredList(responseTimes, crabSubaddress)
+        )
+    )
+  }
+
   private class CrabName(listName: String) extends RegistryName("crab") {
     override val list = s"$name/$listName"
+    override val filteredList = s"$name/filtered$listName"
   }
 }
