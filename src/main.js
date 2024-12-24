73d33a3faf99dc8c7dbdfc51955a1d8c78db387e
Arne Dumarey <dumarey.arne@gmail.com>
2024-12-24 11:53:03 +0100

    ci: add environment

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { group } from 'k6';
+import { BASE_URL } from './config.js';
 import municipalityTest from './municipality/main.js';
 
 export default function () {
+  console.log(`BASE_URL: ${BASE_URL}`);
   group('municipality', () => {
     municipalityTest();
   });
